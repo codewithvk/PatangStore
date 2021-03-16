d453d61334d8fe765bba27039151beb771a6a113
codewithvk <vivekpatel7202@gmail.com>
2021-03-16 10:13:33 +0530

    Show Dummy data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 function App() {
   return (
     <>
        <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome To PatangStore</h1>
+        <HomeScreen />
         </Container>
       </main>
       <Footer />
