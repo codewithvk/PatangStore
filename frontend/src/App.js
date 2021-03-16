54a1c8f03c7df9b566f7470f065829831096d27c
codewithvk <vivekpatel7202@gmail.com>
2021-03-16 21:57:30 +0530

    Home page design done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import ProductScreen from './screens/ProductScreen'
 import HomeScreen from './screens/HomeScreen'
 function App() {
   return (
-    <>
+    <Router>
        <Header />
       <main className='py-3'>
         <Container>
-        <HomeScreen />
+        <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+      </Router>
   );
 }
 
