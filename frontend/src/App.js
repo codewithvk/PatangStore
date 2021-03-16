cbffeaeb1189fd02cec359747bf3f03bb3a50205
codewithvk <vivekpatel7202@gmail.com>
2021-03-16 10:05:04 +0530

    Navbar and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
-
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 function App() {
   return (
-    <div>
-      Hello world
-    </div>
+    <>
+       <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To PatangStore</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
