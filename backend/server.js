a0dd18365b4e8782bf64056333b85b9fe575c2dd
codewithvk <vivekpatel7202@gmail.com>
2021-03-17 09:55:57 +0530

    add product in file

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import products from './data/products.js'
-
+import colors from 'colors'
+import connectDB from './config/db.js'
 dotenv.config()
+connectDB()
 const app = express()
 
 app.get('/', (req, res) => {
@@ -22,5 +24,7 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
