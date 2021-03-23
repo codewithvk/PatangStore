06e202f60120c02b81f14ef4b58033dc311332ed
codewithvk <vivekpatel7202@gmail.com>
2021-03-23 17:19:55 +0530

    Login reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,10 +6,13 @@ import {
     productListReducer,
     productDetailsReducer,
 } from './reducers/productReducers'
+
+import {userLoginReducer} from './reducers/userReducers'
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin : userLoginReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
