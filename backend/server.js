d23fb6d99b3bbf9fec64012db2275e8176cd6501
codewithvk <vivekpatel7202@gmail.com>
2021-04-27 10:38:16 +0530

    Setup for deploy:)

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import path from 'path'
+import path from "path";
 import dotenv from "dotenv";
 import morgan from "morgan";
 // import products from './data/products.js'
@@ -9,7 +9,7 @@ import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/OrderRoutes.js";
 import connectDB from "./config/db.js";
-import uploadRoutes from './routes/uploadRoutes.js'
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -17,31 +17,39 @@ const app = express();
 
 // For see API call in LOG
 
-  if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
-  }
-
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // In letest version of node not need to body parser seprate.
 // It has been combine with express middleware.
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("SERVER IS RUNNING!!!!!");
-});
+// app.get("/", (req, res) => {
+//   res.send("SERVER IS RUNNING!!!!!");
+// });
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
-app.use('/api/upload', uploadRoutes)
+app.use("/api/upload", uploadRoutes);
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get("/", (req, res) => {
+    res.send("SERVER IS RUNNING!!!!!");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
